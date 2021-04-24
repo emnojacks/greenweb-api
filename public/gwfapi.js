c1bf8be7d3315db7acf3403073b716b4983151aa
Emily Jackson <emj@Emilys-Air.lan>
2021-04-24 16:25:13 -0400

    third push fixed conditional bugs

--- public/gwfapi.js
+++ public/gwfapi.js
@@ -28,26 +28,40 @@ function fetchResults(event) {
     fetch(finalURL)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            console.log(data);
+            console.log(data.data);
             introResultsPara.innerHTML = `The url ${appendURL} recieves the following rating from Green Web Foundation:`;
             liName.innerHTML = `URL name <br> ${data.url}`;
             liHost.innerHTML = `<br> Hosted by <br> ${data.hostedby}`;
             liGreen.innerHTML = `<br> Is this website and webhost green? <br> ${data.green}`;
-            if (data.green === true) {
-                resultsList.style.display = "true"
-                resultsList.style.color = "teal"
-                imageTrue.src = "true.png"
-                imageFalse.src = " "
-            } else if (data.green === false) {
-                resultsList.style.display = "true"
-                resultsList.style.color = "red"
-                imageTrue.src = " "
-                imageFalse.src = "false.png"
-            } else {
+            if (data.data === false) {
                 resultsList.style.color = "whitesmoke"
-                imageFalse.style.display = "none";
-                imageTrue.style.display = "none";
+                    //resultsList.style.display = "none"
+                imageTrue.src = " "
+                imageFalse.src = " "
+                liGreen.innerHTML = `<br> Is this website and webhost green? <br> unknown`;
+                introResultsPara.innerHTML = "There was an error fetching your request. A couple things could be happening. <br><br> 1) Check that the spelling of your URL is correct. <br> 2) Green Web Foundations does not have all website urls in their database - especially newer sites with low traffic. <br><br> Fix the spelling or test a more well known site. ";
+            } else if (data.data === true) {
+                resultsList.style.display = "true"
+                imageFalse.style.display = "true";
+                imageTrue.style.display = "true";
+                if (data.green === true) {
+                    resultsList.style.display = "true"
+                    resultsList.style.color = "teal"
+                    imageFalse.src = " "
+                    imageTrue.src = "true.png"
+                        //imageTrue.style.display = "true"
+                        //imageFalse.style.display = "none"
+                } else if (data.green === false) {
+                    resultsList.style.display = "true"
+                    resultsList.style.color = "red"
+                    imageTrue.src = " "
+                    imageFalse.src = "false.png"
+                        //imageTrue.style.display = "none"
+                        //imageFalse.style.display = "true"
+                }
             }
+
         })
         .catch(function(e) {
             console.error(e);
